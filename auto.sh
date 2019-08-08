3cbf7596b5df184a1c01fcabc0be4004a07d025a
ning.wang <ning.wang@zhan.com>
2019-08-08 17:43:51 +0800

    修改shell

--- auto.sh
+++ auto.sh
@@ -9,7 +9,7 @@ do
 	echo "构建"$var"镜像"
 	dir="../"$var"/target/"
 	cp $dir/*.jar ./
-	docker build -t ningxue/$var .
+	docker build -t "ningxue/"$var .
 	echo "构建"$var"镜像完成"
         rm ./*jar
 done
@@ -25,7 +25,7 @@ echo "发布goods服务"
 kubectl create -f ./doc/goods-deployment.yaml
 echo ”发布k8s服务结束...“
 echo "构建路由规则ingress..."
-kubecet create -f ./doc/ingress-order.yaml
+kubectl create -f ./doc/ingress-order.yaml
 echo "构建路由规则结束..."
 echo "SUCCESS"
 
